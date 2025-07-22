4e5fe106f6717aeacd195f5d8e92f1a9c9e80602
basamvarun <165558588+basamvarun@users.noreply.github.com>
2025-07-22 19:46:53 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,34 @@
-// redisaiagent.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
+#include <string>
+#include <winsock2.h>
+#include "redisserver.h"
+#include<thread>
+#include<chrono>
 
-#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+int main(int argc, char* argv[]) {
+	WSADATA wsaData;
+	int wsaInit = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (wsaInit != 0) {
+		std::cerr << "WSAStartup failed. Error: " << wsaInit << "\n";
+		return 1;
+	}
+	int port = 6379;
+	if (argc >= 2) {
+		port = std::stoi(argv[1]);
+	}
+	redisserver server(port);
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    //backgrounud dump for 300 ever seconds
+	std::thread persistanceThread([]() {
+		while (true) {
+			std::this_thread::sleep_for(std::chrono::seconds(300));
+			//dump the database
+		}
+		});
+	persistanceThread.detach();
+	server.run();
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
